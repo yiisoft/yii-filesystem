7db84552b08f03df377599b2a9b99678afd9bbd1
yiiliveext <yiiliveext@gmail.com>
2020-03-12 16:33:40 +0200

    Add filesystem

---
+++ src/FileStorageServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Yiisoft\Yii\Filesystem;
+
+use League\Flysystem\FilesystemAdapter;
+use Yiisoft\Di\Container;
+use Yiisoft\Di\Support\ServiceProvider;
+use Yiisoft\Factory\Factory;
+
+final class FileStorageServiceProvider extends ServiceProvider
+{
+    public function register(Container $container): void
+    {
+        $factory = new Factory();
+        $configs = $container->get(FileStorageConfigs::class)->getConfigs();
+        foreach ($configs as $alias => $config) {
+            $this->validateAdapter($alias, $config);
+            $configParams = $config['config'] ?? [];
+            $aliases = $config['aliases'] ?? [];
+            $adapter = $factory->create($config['adapter']);
+            $container->set($alias, fn () => new Filesystem($adapter, $aliases, $configParams));
+        }
+    }
+
+    private function validateAdapter(string $alias, array $config)
+    {
+        $adapter = $config['adapter']['__class'] ?? false;
+        if (!$adapter) {
+            throw new \RuntimeException("Adapter is not defined in the '$alias' storage config.");
+        }
+        if (!is_subclass_of($adapter, FilesystemAdapter::class)) {
+            throw new \RuntimeException("Adapter must implements FilesystemAdapterInterface.");
+        }
+    }
+}
